21cabe9db91988ec00901666eaafa26f619dcf33
niekt0 <niekt0@kyberia.cz>
2010-10-27 23:39:38 +0200

    foxed K cron script

--- cron/k.php
+++ cron/k.php
@@ -1,8 +1,19 @@
 #!/usr/bin/php
 <?php
-require('/storage/system/www-root/kyberia.sk/config/config.inc');
-require(SYSTEM_ROOT.'/inc/database.inc');
-require(SYSTEM_ROOT.'/inc/senate.inc');
+/*
+ * Script that restores number of K for all users.
+ * Called once a day.
+ */
+
+
+// Use relative address of config file
+// Change this, if you move you cron directory.
+$dir=substr(__FILE__, 0, strrpos(__FILE__, '/'));
+require($dir.'/../wwwroot/config/config.inc');
+
+require('../config/config.inc');
+require(INCLUDE_DIR.'/database.inc');
+require(INCLUDE_DIR.'/senate.inc');
 
 $db = new CLASS_DATABASE();
 $db->query('update users set user_k = '. DAILY_K);
