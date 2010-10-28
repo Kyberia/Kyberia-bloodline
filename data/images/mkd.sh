17c41f4f1de58bd7db4d1cbb11f96de235669cb4
niekt0 <niekt0@kyberia.cz>
2010-10-28 23:17:19 +0200

    added image for first node and modified mkdirectory script

--- data/images/mkd.sh
+++ data/images/mkd.sh
@@ -1,10 +1,10 @@
-mkdir 0
-mkdir 1
-mkdir 2
-mkdir 3
-mkdir 4
-mkdir 5
-mkdir 6
-mkdir 7
-mkdir 8
-mkdir 9
+#!/bin/bash
+
+for ((i=0;i<10;i=i+1)); do
+	mkdir "$i";
+	cd "$i";
+	for ((j=0;j<10;j=j+1)); do
+		mkdir "$j";
+	done
+	cd ..;
+done
