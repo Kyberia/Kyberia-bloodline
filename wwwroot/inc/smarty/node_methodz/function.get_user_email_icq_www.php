0379600442741acb8550923673547c0689d265ce
niekt0 <niekt0@kyberia.cz>
2010-11-03 00:32:31 +0100

    ICQ removed

--- wwwroot/inc/smarty/node_methodz/function.get_user_email_icq_www.php
+++ wwwroot/inc/smarty/node_methodz/function.get_user_email_icq_www.php
@@ -2,14 +2,16 @@
 function smarty_function_get_user_email_icq_www($params,&$smarty) {
     $user_id = $params['user_id'];
     global $db;
-    $q="select users.email,icq,www from users where users.user_id=$user_id";
+//    $q="select users.email,icq,www from users where users.user_id=$user_id";
+    $q="select email from users where user_id=$user_id";
+
     $set=$db->query($q);
     $set->next();
     $email=$set->getString('email');
-    $icq=$set->getString('icq');
-    $www=$set->getString('www');
+//    $icq=$set->getString('icq');
+//    $www=$set->getString('www');
     $smarty->assign('get_user_email',$email);
-    $smarty->assign('get_user_icq',$icq);
-    $smarty->assign('get_user_www',$www);
+//    $smarty->assign('get_user_icq',$icq);
+//    $smarty->assign('get_user_www',$www);
 }
 ?>
