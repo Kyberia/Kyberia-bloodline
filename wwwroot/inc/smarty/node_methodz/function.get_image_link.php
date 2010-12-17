819e561767f728e36b0b4205a2022ba224c847fd
Harvie <tomas@mudrunka.cz>
2010-12-17 13:40:16 +0100

    Fixed inconsistency in image check

--- wwwroot/inc/smarty/node_methodz/function.get_image_link.php
+++ wwwroot/inc/smarty/node_methodz/function.get_image_link.php
@@ -2,7 +2,7 @@
 function smarty_function_get_image_link($params,&$smarty) {
 	global $db;
 	$id = $params['id'];
-	$img = SYSTEM_DATA.SYSTEM_IMAGES.'/nodes/'.substr($id,0,1)."/".substr($id,1,1)."/$id.gif";
+	$img = './'.SYSTEM_IMAGES.'/nodes/'.substr($id,0,1)."/".substr($id,1,1)."/$id.gif";
 
 	if (file_exists($img)) {
 		echo SYSTEM_IMAGES.'/nodes/'.substr($id,0,1)."/".substr($id,1,1)."/$id.gif";
