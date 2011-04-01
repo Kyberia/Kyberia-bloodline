b17a1e9bc746b7eb0549dfd8dff61e132c9de828
niekt0 <niekt0@kyberia.cz>
2011-04-02 01:13:45 +0200

    k_neurons fix & small warnings removed

--- wwwroot/backend/mysql/functions.sql
+++ wwwroot/backend/mysql/functions.sql
@@ -20,17 +20,19 @@ BEGIN
 		/* weigths from user to: 
 			- all nodes from node to root node
 			- all node owners from node to root node
-			- all nodes between themselves (safe?)
+			- all nodes between themselves (safe?) XXX removed
 		*/
-		select node_weight,node_creator into n_weight,n_owner from nodes where node_id=node2;
+		select /*node_weight,*/node_creator into /*n_weight,*/n_owner from nodes where node_id=node2;
 		select synapse_weight into s_weight from neurons where src=user and dst=node2;
+		if found_rows() = 0  then set s_weight=1; end if;
 		select synapse_weight into o_weight from neurons where src=user and dst=n_owner;
+		if found_rows() = 0 then set o_weight=1; end if;
+	
+/*		if o_weight = NULL or o_weight=0 then set o_weight=1; end if;
+		if s_weight = NULL or s_weight=0 then set s_weight=1; end if;*/
+		/*if n_weight = NULL or n_weight=0 then set n_weight=1; end if;*/
 		
-		if o_weight = NULL or o_weight=0 then set o_weight=1; end if;
-		if s_weight = NULL or s_weight=0 then set s_weight=1; end if;
-		if n_weight = NULL or n_weight=0 then set n_weight=1; end if;
-		
-		set final = final * s_weight * o_weight * n_weight;
+		set final = final * s_weight * o_weight;/* * n_weight;*/
 	END WHILE;
 	RETURN final;
 END//
@@ -43,6 +45,7 @@ BEGIN
 		nodes.node_name,
 		nodes.node_creator,
 		nodes.node_content,
+		nodes.k,
 		nodes.node_parent,(k_get_node_weigth(nodes.node_id,user_id)*nodes.k) as weight_k,
 		users.login as creator_name,
 		parent.node_name as parent_name 
