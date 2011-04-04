37e8d23972984a127bec7c4faa237e5a80e1068e
niekt0 <niekt0@kyberia.cz>
2011-04-04 22:49:47 +0200

    get_image_link_fast fix

--- wwwroot/inc/smarty/node_methodz/function.get_image_link_fast.php
+++ wwwroot/inc/smarty/node_methodz/function.get_image_link_fast.php
@@ -7,9 +7,9 @@ function smarty_function_get_image_link_fast($params,&$smarty) {
 	$id = $params['id'];
 	
 	if (!is_numeric($id)) { $id=0;}
-	$img = './'.SYSTEM_IMAGES.'/nodes/'.substr($id,0,1)."/".substr($id,1,1)."/$id.gif";
+	$img = SYSTEM_IMAGES.'nodes/'.substr($id,0,1)."/".substr($id,1,1)."/$id.gif";
 
-	if (file_exists($img)) { echo "$img"; } 
-	else { echo SYSTEM_IMAGES.'/nodes///.gif'; }
+	if (file_exists('./'.$img)) { echo "$img"; } 
+	else { echo SYSTEM_IMAGES.'nodes/.gif'; }
 }
 ?>
