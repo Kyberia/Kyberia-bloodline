229a00474645760cb16603d172f13d6270d9200d
niekt0 <niekt0@kyberia.cz>
2011-06-22 16:07:02 +0200

    getUserSubmissionChildre fix

--- wwwroot/inc/smarty/node_methodz/function.get_last.php
+++ wwwroot/inc/smarty/node_methodz/function.get_last.php
@@ -1,13 +1,11 @@
 <?php
 
 // Get last submissions that appeared on kyberia (all users)
-// XXX permission checking?
 
 function smarty_function_get_last($params,&$smarty) {
 
 	$last=nodes::GetLast($params/*$listing_ammount,$offset,$order_by*/);
 	$smarty->assign('get_last', $last);
-
-
 }
+
 ?>
