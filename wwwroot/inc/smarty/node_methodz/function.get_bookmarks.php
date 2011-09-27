202c37b7ea3edb43a2e45b769aab089effb009f6
niekt0 <niekt0@kyberia.cz>
2011-09-28 01:08:00 +0200

    warnings cleanup

--- wwwroot/inc/smarty/node_methodz/function.get_bookmarks.php
+++ wwwroot/inc/smarty/node_methodz/function.get_bookmarks.php
@@ -2,12 +2,12 @@
 
 function smarty_function_get_bookmarks($brawco,&$smarty) {
 
-if (is_numeric($brawco['node_id'])) $bookcat=$brawco['node_id'];
-if (is_numeric($brawco['user_id'])) $user_id=$brawco['user_id'];
+if ((!empty($brawco['node_id'])) && is_numeric($brawco['node_id'])) $bookcat=$brawco['node_id'];
+if ((!empty($brawco['user_id'])) && is_numeric($brawco['user_id'])) $user_id=$brawco['user_id'];
 global $db;
 
 //returning user specific bookmarks
-if ($user_id) {
+if ((!empty($user_id)) && $user_id) {
 $q="select * from node_access left join nodes on node_access.node_id=nodes.node_id where user_id='$user_id' and node_bookmark='yes' and nodes.node_system_access='public'";
 $set=$db->query($q);
 while ($set->next()) {
@@ -25,31 +25,31 @@ $categories[$result->getString('node_id')]=$result->getRecord();
 }
 
 $q="select * from node_access left join nodes on node_access.node_id=nodes.node_id where user_id='".$_SESSION['user_id']."' and node_bookmark='yes' and node_name is not NULL ";
-if ($bookcat) $q.=" and (bookmark_category='$bookcat' or bookmark_category=0 or bookmark_category IS NULL) order by node_name desc";
-if ($bookcat && $_SESSION['user_id']==548) echo $q;
+if ((!empty($bookcat)) && $bookcat) $q.=" and (bookmark_category='$bookcat' or bookmark_category=0 or bookmark_category IS NULL) order by node_name desc";
+#if ($bookcat && $_SESSION['user_id']==548) echo $q; ble
 
 else $q.="order by node_name";
 $result=$db->query($q);
 if (!$bookcat) {
-while ($result->next()) {
-if (!$result->getString('bookmark_category')) {
-$categories['unsorted']['children'][]=$result->getRecord();
-$categories['unsorted']['sum']+=$result->getString('node_user_subchild_count');
-}
-else {
-$categories[$result->getString('bookmark_category')]['children'][]=$result->getRecord();
-$categories[$result->getString('bookmark_category')]['sum']+=$result->getString('node_user_subchild_count');
-}
+	while ($result->next()) {
+		if (!$result->getString('bookmark_category')) {
+			$categories['unsorted']['children'][]=$result->getRecord();
+			$categories['unsorted']['sum']+=$result->getString('node_user_subchild_count');
+		}
+		else {
+			$categories[$result->getString('bookmark_category')]['children'][]=$result->getRecord();
+			$categories[$result->getString('bookmark_category')]['sum']+=$result->getString('node_user_subchild_count');
+		}
+	}
+
+	$smarty->assign('get_bookmarks',$categories);
 }
 
-$smarty->assign('get_bookmarks',$categories);
-}
-
 else {
-while ($result->next()) {
-$bookmarks[]=$result->getRecord();
-}
-$smarty->assign('get_bookmarks',$bookmarks);
+	while ($result->next()) {
+		$bookmarks[]=$result->getRecord();
+	}
+	$smarty->assign('get_bookmarks',$bookmarks);
 }
 }
 ?>
