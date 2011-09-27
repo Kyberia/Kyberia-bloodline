38927c4d48865f73d1b965f1a07c76efe4339a9a
niekt0 <niekt0@kyberia.cz>
2011-09-28 01:16:07 +0200

    warnings cleanup

--- wwwroot/inc/smarty/node_methodz/function.get_movement_params.php
+++ wwwroot/inc/smarty/node_methodz/function.get_movement_params.php
@@ -1,45 +1,44 @@
 <?php
 
-	function smarty_function_get_movement_params($params,&$smarty) {
-		$children_count=$params['children_count'];
+function smarty_function_get_movement_params($params,&$smarty) {
 
-		if (isset($_POST['listing_amount']) && 
-		(is_numeric($_POST['listing_amount']))) {
-			$listing_amount=$_POST['listing_amount'];
-		} elseif (!empty($_SESSION['listing_amount'])) {
-			$listing_amount=$_SESSION['listing_amount'];
-		} else $listing_amount=DEFAULT_LISTING_AMOUNT;
+	if (isset($_POST['listing_amount']) && 
+	(is_numeric($_POST['listing_amount']))) {
+		$listing_amount=$_POST['listing_amount'];
+	} elseif (!empty($_SESSION['listing_amount'])) {
+		$listing_amount=$_SESSION['listing_amount'];
+	} else $listing_amount=DEFAULT_LISTING_AMOUNT;
 
-		$smarty->assign('listing_amount',$listing_amount);
+	$smarty->assign('listing_amount',$listing_amount);
 
-		if (isset($_POST['get_children_offset']) && 
-		(is_numeric($_POST['get_children_offset']))) {
-			$offset=$_POST['get_children_offset'];
+	if (isset($_POST['get_children_offset']) && 
+	(is_numeric($_POST['get_children_offset']))) {
+		$offset=$_POST['get_children_offset'];
 
-			//movement forward and backward
-			if ($_POST['get_children_move']=='<') {
-				$offset=$offset-$listing_amount;
-				if ($offset<0) $offset=0;
-			}
-			elseif ($_POST['get_children_move']=='>') {
-
-				$offset=$offset+$listing_amount;
-			}
-
-			elseif ($_POST['get_children_move']=='>>') {
-				$offset=$children_count-$listing_amount;
-				if ($offset<0) $offset=0;
-			}
-
-			elseif ($_POST['get_children_move']=='<<') {
-				$offset=0;
-			}
-
-
+		//movement forward and backward
+		if ($_POST['get_children_move']=='<') {
+			$offset=$offset-$listing_amount;
+			if ($offset<0) $offset=0;
 		}
 
-		else $offset=0;
-		$smarty->assign('offset',$offset);
+		elseif ($_POST['get_children_move']=='>') {
+			$offset=$offset+$listing_amount;
+		}
+
+		elseif ($_POST['get_children_move']=='>>') {
+			$offset=$children_count-$listing_amount;
+			if ($offset<0) $offset=0;
+		}
+
+		elseif ($_POST['get_children_move']=='<<') {
+			$offset=0;
+		}
+
 
 	}
+
+	else $offset=0;
+	$smarty->assign('offset',$offset);
+
+}
 ?>
