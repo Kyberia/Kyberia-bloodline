bb1dde27605f903685e3eccaf32c3e399b0cbb7c
Harvie <tomas@mudrunka.cz>
2012-05-29 14:31:45 +0200

    Trasovatelnejsi templaty

--- wwwroot/inc/smarty/resource.kyberia.php
+++ wwwroot/inc/smarty/resource.kyberia.php
@@ -17,9 +17,9 @@ function db_get_template ($tpl_name, &$tpl_source, &$smarty_obj) {
       if (is_numeric($template_id)) {
 	$tpl_source = nodes::getNodeById($template_id,empty($_SESSION['user_id']) ? "" : $_SESSION['user_id']);
       }
-	$tpl_source = "\n<!-- BEGIN TEMPLATE $template_id  -->\n".
+	$tpl_source = "\n\n<!-- BEGIN TEMPLATE $template_id  -->\n\n".
 		$tpl_source['node_content'].
-		"\n<!-- END TEMPLATE $template_id  -->\n";      
+		"\n\n<!-- END TEMPLATE $template_id  -->\n\n";
 
     // return true on success, false to generate failure notification
      return (bool)$tpl_source;
